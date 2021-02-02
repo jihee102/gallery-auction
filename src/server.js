087c9d28b8018bc6ce54cfc630c73d9ddc014e46
Jihee Hong <486480@student.saxion.nl>
2021-02-02 22:48:16 +0100

    cors added

--- src/server.js
+++ src/server.js
@@ -6,15 +6,17 @@ const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
 import init from './server/index';
+import cors from 'cors';
 
 const app = init();
 
 app // You can also use Express
-	.use(
-		compression({ threshold: 0 }),
-		sirv('static', { dev }),
-		sapper.middleware()
-	)
-	.listen(PORT, err => {
-		if (err) console.log('error', err);
-	});
+  .use(
+    compression({ threshold: 0 }),
+    sirv('static', { dev }),
+    sapper.middleware()
+  )
+  .use(cors())
+  .listen(PORT, (err) => {
+    if (err) console.log('error', err);
+  });
