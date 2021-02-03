fb7de1eabb29e7e7d002f144edca3aed6917f5b2
Jihee Hong <486480@student.saxion.nl>
2021-02-03 11:03:18 +0100

    cors -v2

--- src/server/index.js
+++ src/server/index.js
@@ -8,6 +8,16 @@ const init = () => {
   // Middlewares
   app.use(express.json());
   app.use(cors());
+  app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json'
+    );
+    next();
+  });
   // Routes
   app.use('/user', user_router);
   app.use('/paintings', painting_router);
